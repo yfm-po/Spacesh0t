afd8185b1c25ebbfad067611f1fe8e0832ea855e
yfm-po <73367326+yfm-po@users.noreply.github.com>
2022-06-06 22:26:04 +0200

    Add files via upload

---
+++ Spacesh0t.c
@@ -0,0 +1,495 @@
+#define _DEFAULT_SOURCE
+#include <ncurses.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+
+#define count(x)	sizeof(x)/sizeof(x[0])
+#define diff(x,y)	abs(x-y)
+
+
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        printf("Give difficulty (1/2/3) !!\n");
+        return 1;
+    }
+    
+    
+    
+    srand(time(NULL));
+    
+    char name[20];
+    
+    printf("Enter your name: ");
+    printf("\n");
+    
+    scanf("%s", name);
+    
+    
+    
+    
+    printf("Welcome to spacesh0t, %s.\n",name);
+    int u; 
+    printf("Press 0 if you want to see the scoreboard, press 1 if you want to play: \n");
+    scanf("%d", &u);
+    
+    if (u == 0)
+    {
+        FILE *f;
+        char c;
+        
+        f = fopen("./highscores.txt","r");
+        c = fgetc(f);
+        while (c != EOF)
+        {
+            printf("%c",c);
+            c = fgetc(f);
+        }
+        printf("\n");
+        fclose(f);
+        sleep(3);
+    }
+    else if (u == 1)
+    {
+        
+    }
+
+
+    
+    
+    printf("In this game you will have to fight kill as many enemy spaceships you can.\n");
+    printf("Every level-up will spawn more and more enemies.\n");
+    printf("You can respawn 5 times, so you have only 5 lives.\n");
+    sleep(3);
+    
+    
+    
+    
+    
+   
+    
+    
+    
+    
+    //initializing screen
+    initscr();
+    
+    //initializing colors
+    start_color();
+    
+    //used as always...
+    noecho();
+    raw();
+    cbreak();
+    timeout(0);
+    
+    //allowing keyboard
+    keypad(stdscr, TRUE);
+    
+    //cursor
+    curs_set(FALSE);
+    
+    nodelay(stdscr, TRUE);
+    
+    init_pair(1,COLOR_BLACK, COLOR_BLACK);
+    init_pair(2,COLOR_BLUE, COLOR_BLUE);
+    init_pair(3,COLOR_RED, COLOR_YELLOW);
+    init_pair(4,COLOR_RED, COLOR_CYAN);
+    init_pair(5,COLOR_BLUE,COLOR_WHITE);
+    init_pair(6,COLOR_RED,COLOR_GREEN);
+    init_pair(7,COLOR_BLACK,COLOR_WHITE);
+    init_pair(8,COLOR_RED,COLOR_RED);
+    init_pair(9,COLOR_GREEN,COLOR_GREEN);    
+    
+    int level = 1;
+    
+    int argument = 0;
+    char *a = argv[1];
+    argument = atoi(a);
+    int splitter;
+    
+    if (argument == 1)
+    {    
+        splitter = 100;
+    }
+    else if (argument == 2)
+    {
+        splitter = 75;
+    }
+    else if (argument == 3)
+    {
+        splitter = 10;
+    }
+    
+    int p;
+    int h;
+    int w;
+    int key;
+    int die;
+    int kills = 0;
+    int deaths = 0;
+    int bullets[50][2];
+    int enemies[50][3];
+    FILE *highscore;
+   
+  
+    
+    
+    p=0;//counter, it will help us in generating the map
+    
+    //starting height of our spaceship
+    h=10;
+    //starting width of our spaceship
+    w = 6;
+    
+    //die bool value, at the start it's false for sure.
+    die = false;
+    
+    memset(bullets, 0, sizeof(bullets));
+    memset(enemies, 0, sizeof(enemies));
+    
+    
+    
+    
+    
+    
+    while (true)
+    {
+    
+        if (deaths == 5)
+        {
+            mvprintw(LINES/2, COLS/2-5, "[ YOU HAVE LOST THE GAME ]");
+            refresh();
+            getchar();
+            break;
+              
+        }
+        
+        
+        usleep(10000);
+        
+        p++;//increasing p 
+        
+        
+        key = getch(); //waiting for user input
+        
+        
+        //switch function for some keys
+        switch(key)
+        {
+            case 'w': case KEY_UP:
+                if (h > 1)
+                {
+                    h--;
+                }
+                break;
+                
+            case 's': case KEY_DOWN:
+                if (h < 25)
+                {
+                    h++;
+                }
+                break;
+                
+            case 'd': case KEY_RIGHT:
+            {
+                if (w < 600)
+                {
+                    w++;
+                }
+                break;
+            }
+            
+            case 'a': case KEY_LEFT:
+            {
+                if (w > 1)
+                {
+                    w--;
+                }
+                break;
+            }
+                
+            case ' ':
+                for(int x = 0; x < count(bullets);x++)
+                {
+                    if(bullets[x][0] == 0)
+                    {
+                        bullets[x][0] = w;
+                        bullets[x][1] = h;
+                        break;
+                    }
+                }
+                break;
+                
+            case 'q' : case 'Q' :
+            { 
+                mvprintw(LINES/2, COLS/2-5, "[ QUIT GAME ]");
+                refresh();
+                while (true)
+                {
+                    int key_press = getch();
+
+                    if (key_press == 'q' || key_press == 'Q')
+                    {
+                        endwin();
+                    }
+                }
+                
+                break;
+                
+                
+            }
+            
+            case 'p' : case 'P' :
+            {
+                mvprintw(LINES/2,COLS/2-5,"[ PAUSED ]");
+                refresh();
+                while (true)
+                {
+                    int key_press = getch();
+
+                    if (key_press == 'p' || key_press == 'P')
+                    {
+                        break;
+                    }
+                }
+                break;
+            }
+         }
+        
+        //generating enemy position
+        if((rand() % splitter) == 0)//the splitter is always being decreased in our loop(when we get a kill, so there will spawn always more enemies if we kill one)
+        {
+            for(int x = 0;x != count(enemies);x++)
+            {
+                if(enemies[x][0] == 0)
+                {
+                    enemies[x][0] = 180;  //the enemies will be spawning from x=180
+                    enemies[x][1] = 7;                                        //y=7
+                  
+                    //spawning the enemies randomly vertically
+                    if (rand() % 2 == 0)
+                    {
+                        enemies[x][1] -= (rand() % 7);
+                    }
+                    else
+                    {
+                        enemies[x][1] += (rand() % 7);
+                    }
+                    break;
+                }
+            }
+        }
+        
+        int n;
+        //generating the map, i have chosen 600x300pixels
+        for(int x = 0;x != 600; x++)
+        {
+            for(int y = 0;y != 300; y++)
+            {
+                y-=20;
+                //'generating' black space with cos
+                
+                if(cos((float)(x+p)/30) > ((float)y / 5))
+                {
+                    n = 1; 
+                }
+                else
+                {
+                    n = 2;
+                    //in the case of collision, we die.
+                    if(x == 16 && (h-18 > y))
+                    {
+                        
+                        die = true;
+                        deaths++;
+                        if (deaths == 5)
+                        {
+                             mvprintw(LINES/2, COLS/2, "[ YOU HAVE LOST THE GAME ]");
+                             refresh();
+                             getchar();
+                             break;  
+                        }
+                        
+                    }
+                }
+                y+=20;
+                
+                attron(COLOR_PAIR(n));
+                mvprintw(y,x,"."); 
+                attroff(COLOR_PAIR(n));
+            }
+        }
+        
+        
+        
+        //Shooting out the bullets
+        for(int x = 0;x != count(bullets); x++)
+        {
+            //if bullets's x'th array's zero'th array isn't zero, we are shooting out the bullet
+            if (bullets[x][0] != 0)
+            {
+                bullets[x][0] += 1;
+                attron(COLOR_PAIR(6));
+                mvprintw(bullets[x][1],bullets[x][0],">");
+            }
+            
+            //the bullet is being deleted if it reaches 200 pixels
+            if(bullets[x][0] > 200)
+            {
+                bullets[x][0] = 0;
+            }
+            
+        }
+        
+        
+        //Drawing the enemies
+        for(int x = 0;x != count(enemies); x++)
+        {
+            if (enemies[x][0] && enemies[x][2] == 0)
+            {
+                enemies[x][0]--;
+            }
+            
+            if (enemies[x][0] == 1)
+            {
+                enemies[x][0] = 0;    
+            }
+            
+            if (enemies[x][2] != 0)
+            {
+                enemies[x][2]--;
+                attron(COLOR_PAIR(3)); 
+                
+                if(enemies[x][2] == 0)
+                {
+                    enemies[x][0] = 0;
+                }
+            }
+            else
+            {
+                
+                attron(COLOR_PAIR(4));
+                
+                //in case of collision we die
+                if(enemies[x][0] != 0 && enemies[x][0] < w && diff(h, enemies[x][1]) < 3)
+                {
+                    if (deaths == 5)
+                    {
+                        mvprintw(LINES/2, COLS/2-5, "[ YOU HAVE LOST THE GAME ]");
+                        refresh();
+                        getchar();
+                        break;    
+                    }
+                    die = true;
+                    deaths++;
+                    if (deaths == 5)
+                    {
+                        mvprintw(LINES/2, COLS/2-5, "[ YOU HAVE LOST THE GAME ]");
+                        refresh();
+                        getchar();
+                        break;
+              
+                    }
+                        attron(COLOR_PAIR(3));
+                        enemies[x][2] = 4;
+                        break;
+                    if (deaths == 5)
+                    {
+                        mvprintw(LINES/2, COLS/2-5, "[ YOU HAVE LOST THE GAME ]");
+                        refresh();
+                        getchar();
+                        break;    
+                    }
+                }
+                
+                for (int y = 0;y != count(bullets);y++)
+                {
+                    //if the bullets hit the enemies, they die and we get a kill
+                    if(bullets[y][0] != 0 && diff(enemies[x][0],bullets[y][0]) < 2 && diff(enemies[x][1],bullets[y][1]) < 2)
+                    {
+                        kills++;
+                        //here we are increasing the amount of kills and increasing the amount of the enemies
+                        if (splitter != 5)
+                        {
+                            splitter--;
+                        }
+                        if (kills % 2 == 0)
+                        {
+                            level++;
+                        }
+                        
+                      
+                        attron(COLOR_PAIR(3));
+                        enemies[x][2] = 4;
+                        break;
+                    }
+                }
+            }
+            
+            if(enemies[x][0])
+            {
+                mvprintw(enemies[x][1]-1,enemies[x][0]+2,"/|\\");
+	            mvprintw(enemies[x][1],enemies[x][0],"<O-==={");
+	            mvprintw(enemies[x][1]+1,enemies[x][0]+2,"\\|/");
+            }
+        }
+        
+        
+        
+        attron(COLOR_PAIR(8));
+        mvprintw(0,177," ##########  ");
+        mvprintw(1,177,"#");
+        attron(COLOR_PAIR(7));
+        mvprintw(1,177," Kills: %d    ", kills);
+        mvprintw(2,177," Level: %d    ", level);
+        mvprintw(3,177," Deaths:%d/5  ", deaths);
+        attron(COLOR_PAIR(9));
+        mvprintw(4,177," ##########  ");
+        
+        
+        if(!die)
+        {
+            attron(COLOR_PAIR(5));
+            mvprintw(h-1,w,"|\\ |\\");
+	        mvprintw(h,w-1,"}======-O)");
+	        mvprintw(h+1,w,"|/ |/");
+	        move(0,0);
+	        refresh();
+        }
+        else if (die)
+        {
+            attron(COLOR_PAIR(3));
+            mvprintw(h-1,w,"|\\ |\\");
+	        mvprintw(h,w-1,"}======-O)");
+	        mvprintw(h+1,w,"|/ |/");
+	        move(0,0);
+	        refresh();
+            sleep(0.5);
+            p=0;
+            h=10;
+            w = 6;
+            die = false;
+            memset(bullets, 0, sizeof(bullets));
+            memset(enemies, 0, sizeof(enemies));
+        }
+    }
+    
+    
+    
+    
+    
+    refresh();
+    endwin();
+    highscore = fopen("./highscores.txt","a");
+    fprintf(highscore, "Name: %s | Level: %d | Kills: %d\n", name, level, kills);
+    fclose(highscore);
+}
